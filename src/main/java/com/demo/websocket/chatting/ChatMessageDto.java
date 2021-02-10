9fd63fa7bce9f27c4972e0508685920b65d88e69
jinheebyeon <wlsgml8989@gmail.com>
2021-02-10 23:12:35 +0900

    # 채팅메세지 구현을 위한 dto작성

---
+++ src/main/java/com/demo/websocket/chatting/ChatMessageDto.java
@@ -0,0 +1,18 @@
+package com.demo.websocket.chatting;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessageDto {
+
+    public enum MessageType{
+        ENTER, TALK //입장,채팅
+    }
+
+    private MessageType type;       //메세지타입
+    private String roomId;          //방번호
+    private String sender;          //보낸사람
+    private String message;         //메세지0
+}
