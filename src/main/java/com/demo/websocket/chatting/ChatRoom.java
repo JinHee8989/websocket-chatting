8761ddf12d005825545ed73dffe0a992a277f9ba
jinheebyeon <wlsgml8989@gmail.com>
2021-02-12 22:20:31 +0900

    # 채팅방 클래스 작성 완료

--- src/main/java/com/demo/websocket/chatting/ChatRoom.java
+++ src/main/java/com/demo/websocket/chatting/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.demo.websocket.chatting;
 
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -10,6 +11,25 @@ import java.util.Set;
 public class ChatRoom {
     private String roomId;
     private String name;
-    private Set<WebSocketSession> session = new HashSet<>();
+    private Set<WebSocketSession> sessions = new HashSet<>();
+
+    @Builder
+    public ChatRoom(String roomId, String name){
+        this.roomId = roomId;
+        this.name = name;
+    }
+
+    public void handlerActions(WebSocketSession session, ChatMessageDto message,ChatService chatService){
+        if(message.getType().equals(ChatMessageDto.MessageType.ENTER)){
+            sessions.add(session);
+            message.setMessage(message.getSender()+"님이 입장하셨습니다.");
+
+        }
+        sendMessage(message,chatService);
+    }
+
+    private void sendMessage(ChatMessageDto message, ChatService chatService) {
+        sessions.parallelStream().forEach(session -> charService.sendMessage(session,message));
+    }
 
 }
